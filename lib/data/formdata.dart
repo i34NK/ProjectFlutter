f9f1712510f51bf34b59e5ea53cede86be513acb
i34NK <122728969+i34NK@users.noreply.github.com>
2024-05-01 21:44:41 +0700

    add function resendOTP

---
+++ lib/data/formdata.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_register/model/ConsentFormModel.dart';
+import 'package:flutter_application_register/api/apiform.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
+
+class FormData extends StatefulWidget {
+  @override
+  _FormDataState createState() => _FormDataState();
+}
+
+class _FormDataState extends State<FormData> {
+  List<Payload> _forms = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadForms();
+  }
+
+  Future<void> _loadForms() async {
+    FetchConsentFormList formDataList = FetchConsentFormList();
+    List<Payload> forms = await formDataList.getConsentFormList();
+    setState(() {
+      _forms = forms;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _forms.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _forms.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Row(
+                        children: [
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '${_forms?[index].formId}',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${_forms?[index].title}',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Type: '
+                                  '${_forms?[index].dataType}',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Date: '
+                                  '${_forms?[index].requestDate}',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.article_outlined),
+                        onPressed: () {
+                          _showMyDialog(context, _forms[index]);
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
+
+Future<void> _showMyDialog(BuildContext context, Payload formData) async {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('${formData.title}'),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(height: 16),
+            Text(
+              '${formData.notes}',
+              style: TextStyle(fontSize: 14),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '${formData.footer}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              showAlert(context);
+            },
+            child: Text('ยกเลิกให้คำยินยอม'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('ตกลง'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+void showAlert(BuildContext context) {
+  QuickAlert.show(context: context, type: QuickAlertType.confirm);
+}
---
+++ lib/page/suspended.dart
@@ -0,0 +1,152 @@
+import 'dart:convert';
+
+import 'package:flutter_application_register/page/sendOTP.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_application_register/page/activitie.dart';
+import 'package:flutter_application_register/page/suspended.dart';
+import 'package:flutter_application_register/model/UserModel.dart';
+import 'package:flutter_application_register/data/suspendedData.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SuspendedPage extends StatefulWidget {
+  const SuspendedPage({Key? key}) : super(key: key);
+
+  @override
+  _SuspendedPageState createState() => _SuspendedPageState();
+}
+
+class _SuspendedPageState extends State<SuspendedPage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 50),
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 10),
+            child: Text(
+              'เอกสารความยินยอมที่ถูกระงับ',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, bottom: 10),
+            child: Text(
+              'รายการเอกสารความยินยอมที่ถูกระงับ',
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'หมายเลข',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'แบบฟอร์มคำยินยอม',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'รายละเอียด',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+              ),
+            ],
+          ),
+          Divider(
+            color: Colors.grey,
+            thickness: 3,
+          ),
+          // Expanded(
+          //   child: SuspendedData(),
+          // ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor:
+            Color.fromARGB(255, 145, 235, 148), 
+            selectedItemColor: Colors.white, 
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType
+            .fixed, 
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article_outlined),
+            label: 'เอกสารคำยินยอม',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description),
+            label: 'ความยินยอมที่ระงับ',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.logout),
+              label: 'ออกจากระบบ',
+            ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (int index) async{
+          setState(() {
+            _selectedIndex = index;
+            if (index == 0) {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => Activitie()));
+            }
+            if (index == 2) {
+                _logout(); // เรียกฟังก์ชัน _logout()
+              }
+          });
+        },
+      ),
+    );
+  }
+  void _logout() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.remove("login");
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => SendOTPPage()),
+      (route) => false,
+    );
+  }
+}
+
+
+Future<void> _showMyDialog(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('ความยินยอมข้อมูลส่วนบุคคล'),
+        content: Text(
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit. Vivamus ipsum est tincidunt sit amet posuere vel, luctus vel dolor. Suspendisse potenti. Curabitur vel mauris vel tortor pellentesque tempor. Pellentesque sodales, erat id congue blandit ipsum metus molestie arcu ac hendrerit felis est id leo. Pellentesque dolor ligula feugiat in diam nec lacinia tristique dui. Donec molestie ex eget purus malesuada egestas. Quisque commodo sagittis ante ac viverra. In id nulla nunc. Nulla elementum eros at vestibulum dignissim. Sed et lacinia est. Mauris non semper sapien. Lorem ipsum dolor sit amet consectetur adipiscing elit'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('ยกเลิกให้คำยินยอม'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('ตกลง'),
+          ),
+        ],
+      );
+    },
+  );
+}
